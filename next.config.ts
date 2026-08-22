8dfd762042d690e8c2a39519224b899572622568
Abhishek Kumar Mahato <abhishekkumar1289198511@gmail.com>
2026-08-22 14:18:10 +0530

    CSS part is done now the v1 is officially completed-error fixed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/api/pdf/**": [
+      "./node_modules/playwright-core/**",
+    ],
+  },
 };
 
 export default nextConfig;
